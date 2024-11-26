751b1d0f74619a43d131fa1130b3de094b5307f5
Khalid1754 <s2105777@student.hb.se>
2024-11-26 17:44:25 +0100

    Commit2

--- MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -16,5 +16,6 @@ public class HelloWorldController : Controller
     public string Welcome(string name, int ID = 1)
     {
         return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        //
     }
 }
