0962aeb5e5ffbe9a3e1dc3dfc99cc34e948014bb
Mohameeddh <s2106555@student.hb.se>
2024-12-02 15:15:14 +0100

    Commit11

--- MvcMovie/MvcMovie/Models/Movie.cs
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -1,14 +1,39 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models;
 
 public class Movie
 {
     public int Id { get; set; }
+
+    [StringLength(65, MinimumLength =1)]
+    [RegularExpression(@"^(?!\s*$).+", ErrorMessage = "Title cannot be empty or whitespace.")]
+    [Required]
     public string? Title { get; set; }
+
+    [StringLength(300, MinimumLength =5)]
+    [RegularExpression(@"^(?!\s*$).+", ErrorMessage = "Description cannot be empty or whitespace.")]
+    [Required]
     public string? Description { get; set; }
+
+    [RegularExpression(@"^(?!\s*$).+", ErrorMessage = "Length cannot be empty or whitespace.")]
+    [Required]
     public TimeSpan Length { get; set; }
+
+    [StringLength(25, MinimumLength =3)]
+    [RegularExpression(@"^(?!\s*$).+", ErrorMessage = "Genre cannot be empty or whitespace.")]
+    [Required]
     public string? Genre { get; set; }
+
+    [Range(120, 180)]
+    [DataType(DataType.Currency)]
+    [Column(TypeName ="decimal(18, 2)")]
+    [RegularExpression(@"^(?!\s*$).+", ErrorMessage = "Price cannot be empty or whitespace.")]
     public decimal Price { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+    [StringLength(5)]
+    [Required]
     public string? Rating { get; set; }
 }
--- MvcMovie/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55797b3af25b792d95eb536c5f9093595630f9f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79a17ec88deb718b8498059214f8a7297fefb06d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
